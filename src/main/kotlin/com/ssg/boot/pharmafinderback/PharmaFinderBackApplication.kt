12614094e88347d7bee271c9d32e5b4c45e0a948
Kim JinSeon <kwlstjs00@gmail.com>
2024-04-30 12:40:29 +0900

    feat: init project

---
+++ src/main/kotlin/com/ssg/boot/pharmafinderback/PharmaFinderBackApplication.kt
@@ -0,0 +1,11 @@
+package com.ssg.boot.pharmafinderback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PharmaFinderBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<PharmaFinderBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/ssg/boot/pharmafinderback/PharmaFinderBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ssg.boot.pharmafinderback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PharmaFinderBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
